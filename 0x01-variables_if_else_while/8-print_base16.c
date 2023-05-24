87a714ca09c91a6ffdc686710d3098d55bbce661
waridiyara <krishnaanita4@gmail.com>
2023-05-24 10:59:33 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints numbers to base 10 starting with 0
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; ch++)
+	{
+		putchar(x);
+	}
+	for (x = 'a'; x <= 'f'; ch++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
