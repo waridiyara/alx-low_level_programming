41b192f9f192995134f37b182578c8924b058fd6
waridiyara <krishnaanita4@gmail.com>
2023-06-01 04:42:01 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha -checks gor lowercase letter
+ * @c: character to be checked
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+}
